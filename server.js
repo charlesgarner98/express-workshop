82489c214e768ad09c35ae0c4a39843d397b55b1
Charles Garner <charles.garner@ada.ac.uk>
2018-11-08 10:26:51 +0000

    Added endpoints for /, /chocolate, /node, /girls

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
 var express = require('express');
+var app = express();
+
+//GET requests
+app.get("/", function (req, res) {
+  res.send("Yay Node Girls!");
+});
+app.get("/chocolate", function (req, res) {
+    res.send("Mm chocolate :O");
+});
+app.get("/node", function (req, res) {
+    res.send("This is the node endpoint");
+});
+app.get("/girls", function (req, res) {
+    res.send("This is the girls endpoint");
+});
+
+//Run server
+app.listen(3000, function () {
+  console.log('Server is listening on port 3000. Ready to accept requests!');
+})
