c3dfff054058fb70818764e0f858521c184c2958
Charles Garner <charles.garner@ada.ac.uk>
2018-11-08 11:47:47 +0000

    Added POST requests

--- server.js
+++ server.js
@@ -1,9 +1,29 @@
-var express = require('express');
-var app = express();
+const express = require('express')
+const app = express()
+const formidable = require('express-formidable')
+const fs = require('fs')
 
-app.use(express.static("public"));
+app.use(express.static("public"))
+app.use(formidable())
+
+//POST requests
+app.post('/create-post', function (req, res) {
+  let file = fs.readFileSync(__dirname + '/data/posts.json', 'utf8')
+  let posts = JSON.parse(file)
+  posts[Date.now()] = req.fields.blogpost
+  let postsJSON = JSON.stringify(posts)
+
+  fs.writeFile(__dirname + '/data/posts.json', postsJSON, function (error) {
+    if (error) {
+      console.log(error)
+    } else {
+      res.sendStatus(200)
+    }
+    console.log("Added New Blog Post: " + JSON.stringify(req.fields))
+  })
+})
 
 //Run server
 app.listen(3000, function () {
-  console.log('Server is listening on port 3000. Ready to accept requests!');
+  console.log('Server is listening on port 3000. Ready to accept requests!')
 })
