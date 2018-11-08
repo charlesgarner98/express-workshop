d51a44b99379bdedd1d184683e045166102e8315
Charles Garner <charles.garner@ada.ac.uk>
2018-11-08 10:29:42 +0000

    Tested serving static files

--- server.js
+++ server.js
@@ -1,19 +1,7 @@
 var express = require('express');
 var app = express();
 
-//GET requests
-app.get("/", function (req, res) {
-  res.send("Yay Node Girls!");
-});
-app.get("/chocolate", function (req, res) {
-    res.send("Mm chocolate :O");
-});
-app.get("/node", function (req, res) {
-    res.send("This is the node endpoint");
-});
-app.get("/girls", function (req, res) {
-    res.send("This is the girls endpoint");
-});
+app.use(express.static("public"));
 
 //Run server
 app.listen(3000, function () {
